6de8dd56c506d990dd9b2d33f293feb209229ea5
vagrant <vagrant@homestead>
2017-11-16 02:47:07 +0000

    Finish layout and routes

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,7 +1,12 @@
 <!DOCTYPE>
 <html>
-<title>Samp App</title>
+<title>@yield('title','Sample')-Laravel入门</title>
+<link rel="stylesheet" href="/css/app.css">
 <body>
+ @include('layouts._header')
+  <div class="container">
     @yield('content')
+  </div>
+  @include('layouts._footer')
 </body>
 </html>
--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,4 +1,13 @@
 @extends('layouts.default')
 @section('content')
-    <h1>首页</h1>
-@stop    
+    <div class="jumbotron">
+       <h1>Hello,World</h1>
+       <p>你现在所看到的是<a href="www.baidu.com">入门教程</a></p>
+       <p>
+         一切，从这里开始
+       </p>
+       <p>
+          <a class="btn btn-lg btn-success" href="{{ route('signup') }}" role="button">现在注册</a>
+       </p>
+    </div>
+@stop
