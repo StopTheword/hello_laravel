ea192bfba1370cd8900e0e77d620f9fddd7fb5c5
vagrant <vagrant@homestead>
2017-11-16 05:32:26 +0000

    Move user model to folder

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,7 +18,7 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
-            $table->rememberToken();
+            $table->rememberToken(); //用于保存记住我的信息
             $table->timestamps();
         });
     }
